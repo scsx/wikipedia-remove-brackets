287320c5c22cb80bc02f578473dd1960a385fc9a
scsx <4182349+scsx@users.noreply.github.com>
2020-12-18 16:03:05 +0000

    Finish

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# wikipedia-remove-brackets
+# Wikipedia remove brackets
+
+Copy and paste a text from wikipedia (or other source) to remove those pesky brackets offending your eyes and readability.
---
+++ src/index.js
@@ -0,0 +1,18 @@
+let textarea1 = document.getElementById("paste");
+let textarea2 = document.getElementById("copy");
+let copybutton = document.getElementById("copybutton");
+
+textarea1.addEventListener('paste', (event) => {
+    let paste = (event.clipboardData || window.clipboardData).getData('text');
+    paste = paste.replace(/(\[.*?\])/g, '');
+    textarea2.value = paste;
+});
+
+copybutton.addEventListener('click', copy);
+
+function copy() {
+  textarea2.select();
+  document.execCommand("copy");
+}
+
+let test = "Neanderthal variants [17][7][8]";
